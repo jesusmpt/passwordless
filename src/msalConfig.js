6beb7095c7c465698769cb170f5781fe1b621dc9
jesusmpt <52034316+jesusmpt@users.noreply.github.com>
2025-11-03 18:22:11 +0100

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -3,7 +3,7 @@ import { PublicClientApplication } from '@azure/msal-browser';
 // REPLACE these values with your App Registration values
 const CLIENT_ID = 'b151075c-0779-42c2-b7aa-fa1bc6c36bd5';
 const TENANT_ID = '9ff87f7c-8358-46b5-88bc-d73c09ce789f';
-const REDIRECT_URI = 'https://thankful-sea-07f4a4a03.3.azurestaticapps.net';
+const REDIRECT_URI = 'https://purple-dune-013e10d03.3.azurestaticapps.net';
 
 export const msalConfig = {
   auth: {
